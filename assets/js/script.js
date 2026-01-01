3446a9fa8ea95b9f878ec9307fd245f463cb7f61
Enoch Yu <247741827+enochyu-official@users.noreply.github.com>
2026-01-01 20:57:34 +0900

    minor changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,3 +79,12 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+
+const currentPath = window.location.pathname;
+const links = document.querySelectorAll('.nav-link');
+
+links.forEach(link => {
+    if (link.getAttribute('href') === currentPath) {
+        link.classList.add('active');
+    }
+});
